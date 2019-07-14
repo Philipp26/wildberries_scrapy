9cde4aa4281c73ea9062af006f645d1e5a32429b
Philipp26 <henrybauers@gmail.com>
2019-07-14 20:04:20 +0400

    Fix price fields

--- wb_scraper/wb_scraper/spiders/wb_spider.py
+++ wb_scraper/wb_scraper/spiders/wb_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 class WildBerriesSpider(scrapy.Spider):
     name = "wb_spider"
@@ -11,16 +12,37 @@ class WildBerriesSpider(scrapy.Spider):
             yield {
                    'stock' : 
                    {
-                        'count' : self.check_price_for_null(item.xpath('.//span/span/span/a/span/span/span/span/text()').extract_first()),
+                        'count' : self.check_count_for_null(item.xpath('.//span/span/span/a/span/span/span/span/text()').extract_first()),
                         'in_stock' : bool(item.xpath('.//span/span/span/a/span/span/span/span/text()').extract_first()) 
                    },
 
-                   'price' : item.xpath('.//span/span/span/a/div[@class="dtlist-inner-brand"]/div[@class="j-cataloger-price"]/span/span[@class="lower-price"]/text()').extract_first(),
+                   'price_data' :
+                   {
+                   'original' : self.parse_value(item.xpath('.//span/span/span/a/div[@class="dtlist-inner-brand"]/div[@class="j-cataloger-price"]/span/span[@class="price-old-block"]/del/text()').extract_first()),
+                   'current' : self.parse_value(item.xpath('.//span/span/span/a/div[@class="dtlist-inner-brand"]/div[@class="j-cataloger-price"]/span/ins/text()').extract_first()),
+                   'sale_tag' : self.check_discount(item.xpath('.//span/span/span/a/div[@class="dt    list-inner-brand"]/div[@class="j-cataloger-price"]/span/span[@class="price-old-block"]/span/text()').extract_first())
+                   },
+
                    'brand' : item.xpath('.//span/span/span/a/div[@class="dtlist-inner-brand"]/div[@class="dtlist-inner-brand-name"]/strong/text()').extract_first(),
                    'section' : response.xpath('//div[@class="breadcrumbs"]/div/a/span/text()').getall()
                    }
 
-    def check_price_for_null(self, extracted_item):
+    def check_count_for_null(self, extracted_item):
         if bool(extracted_item):
             return int(extracted_item)
         return 0
+
+    def parse_value(self, extracted_item):
+        if bool(extracted_item):
+            return float(re.sub('[^0-9]', '', extracted_item))
+
+    def check_discount(self, discount):
+        return discount
+        if bool(discount):
+            return 'Discount is {}%'.format(int(parse_value(extracted_item)))
+        return 0
+
+
+
+
+
